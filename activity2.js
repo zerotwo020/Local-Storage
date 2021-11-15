fd0b97ffacec7597c95c38993494e9de2ea0704c
zerotwo020 <85903183+zerotwo020@users.noreply.github.com>
2021-11-15 17:30:25 -0500

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,8 @@
+function back() {
+    window.location = "activity_1.html" 
+}
+
+function getScore() {
+    score = localStorage.getItem("score")
+    document.getElementById("update").innerHTML="<h1> Score:" + score + "</h1>"
+}
